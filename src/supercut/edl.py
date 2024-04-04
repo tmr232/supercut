5c402b034b21954a59334f2f2f051837c0ab143f
Tamir Bahar <tamir.bahar@gmail.com>
2024-04-04 13:09:12 +0300

    Updated to better match openshot

--- src/supercut/edl.py
+++ src/supercut/edl.py
@@ -1,3 +1,4 @@
+import math
 from pathlib import Path
 
 from supercut.ffmpeg import VideoPart
@@ -5,13 +6,13 @@ from supercut.ffmpeg import VideoPart
 
 
 def format_time(time_ms:int)->str:
-    subsecond = (time_ms % 1000) // 10
+    frame = round((time_ms % 1000) / 1000 * 23.98)+1
     total_seconds = time_ms // 1000
     seconds = total_seconds % 60
     minutes = (total_seconds // 60) % 60
     hours = (total_seconds // 3600)
 
-    return f"{hours:02}:{minutes:02}:{seconds:02}:{subsecond:02}"
+    return f"{hours:02}:{minutes:02}:{seconds:02}:{frame:02}"
 
 
 def write_edl(title:str, parts:list[VideoPart]):
@@ -21,13 +22,14 @@ def write_edl(title:str, parts:list[VideoPart]):
     edl_lines.append(title)
 
     record_in_ms = 0
-    for i, part in enumerate(parts):
+    for i, part in enumerate(parts, start=1):
         edl_lines.append("")
 
         timing = f"{format_time(part.start)} {format_time(part.end)} {format_time(record_in_ms)} {format_time(record_in_ms + part.duration_ms)}"
 
-        video = f"{i:03} BL V C {timing}"
-        audio = f"{i:03} AX A C {timing}"
+        # Use only the AX tape as OpenShot ignores the BL tape.
+        video = f"{i:03}  AX       V     C        {timing}"
+        audio = f"{i:03}  AX       A     C        {timing}"
         source = f"* FROM CLIP NAME: {part.video!s}"
 
         edl_lines.append(video)
